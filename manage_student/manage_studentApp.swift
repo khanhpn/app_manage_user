e35df12438635442fb67e577abc755fdef42bd92
khanhpn <phamkhanh90@gmail.com>
2024-05-24 22:02:38 +0700

    Initial Commit

---
+++ manage_student/manage_studentApp.swift
@@ -0,0 +1,17 @@
+//
+//  manage_studentApp.swift
+//  manage_student
+//
+//  Created by khanh on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct manage_studentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
